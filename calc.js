cd88feee70d0034871dee22aea052ebb0800aa57
0x3cc-dev <265554731+0x3cc-dev@users.noreply.github.com>
2026-03-25 15:43:51 -0400

    Make number buttons functional

--- calc.js
+++ calc.js
@@ -27,6 +27,41 @@ function operate(operator, a, b) {
     }
 }
 
+function handleButtonPressed(button) {
+    switch (button) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            handleNumberPressed(button);
+            break;
+    }
+}
+
+function handleNumberPressed(number) {
+    display.textContent += number;
+}
+
+function setupUI() {
+    const buttons = document.querySelectorAll(".buttons button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            handleButtonPressed(button.textContent);
+        });
+    });
+}
+
 let operand1 = null;
 let operand2 = null;
 let currentOperator = null;
+
+const display = document.querySelector(".display");
+display.textContent = "";
+
+setupUI();
